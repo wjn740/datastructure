06399295b606fda32438fc9772e27969ff1431d9
James Wang <jnwang@suse.com>
2017-05-03 18:06:14 +0800

    Add stack case

--- table/linklist/list.c
+++ table/linklist/list.c
@@ -31,5 +31,59 @@ Find(ElementType X, List L)
 void Delete(ElementType X, List L)
 {
     Position P, TmpCell;
+    P = FindPrevious(X, L);
+    if (!IsLast(P,L)) {
+        TmpCell = P->Next;
+        P->Next = TmpCell->Next;
+        free(TmpCell);
+    }
 
+}
+
+Position FindPrevious(ElementType X, List L)
+{
+    Position P;
+    P=L;
+    while(P->Next!=NULL && P->Next->Element != X)
+        P=P->Next;
+    return P;
+}
+void Insert(ElementType X, List L, Position P)
+{
+    Position TmpCell;
+    TmpCell = malloc(sizeof(struct Node));
+    if (TmpCell == NULL) {
+        printf("Out of space!!!\n");
+        exit(1);
+    }
+    TmpCell->Element = X;
+    TmpCell->Next = P->Next;
+    P->Next = TmpCell;
+}
+void DeleteList(List L)
+{
+    Position P, Tmp;
+    P=L->next;
+    L->next=NULL;
+    while(P != NULL) {
+        Tmp = P->Next;
+        free(P);
+        P
+    }
+}
+Position Header(List L)
+{
+    return L;
+}
+Position First(List L)
+{
+    return L->Next;
+}
+Position Advance(Position P)
+{
+    return P->Next;
+}
+ElementType Retrieve(Position P)
+{
+    return P->Element;
 }
---
+++ table/stack/stack.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "stack.h"
+#include <stdlib.h>
+
+struct Node
+{
+    ElementType Element;
+    PtrToNode Next;
+};
+
+int
+IsEmpty(Stack S)
+{
+    return S->Next == NULL;
+}
+
+Stack
+CreateStack(void)
+{
+    Stack S;
+    S = malloc(sizeof(struct Node));
+    if(S==NULL) {
+        printf("Out of space!!!\n");
+        exit(1);
+    }
+    S->Next = NULL;
+    MakeEmpty(S);
+    return S;
+}
+void
+MakeEmpty(Stack S)
+{
+    if(S==NULL) {
+        printf("Must use CreateStack first\n");
+        exit(1);
+    }else {
+        while(!IsEmpty(S))
+            Pop(S);
+    }
+}
+void
+Push(ElementType X, Stack S)
+{
+    PtrToNode TmpCell;
+    TmpCell = malloc(sizeof(struct Node));
+    if (TmpCell == NULL) {
+        printf("Out of space!!!\n");
+        exit(1);
+    }else {
+        TmpCell->Element = X;
+        TmpCell->Next = S->Next; /*previous insert*/
+        S->Next = TmpCell;
+    }
+}
+ElementType
+Top(Stack S)
+{
+    if (!IsEmpty(S)) {
+        return S->Next->Element;
+    }
+    printf("Empty Stack\n");
+    return 0;
+}
+void
+Pop(Stack S)
+{
+    PtrToNode FirstCell;
+    if(IsEmpty(S)) {
+        printf("Empty stack\n");
+        exit(1);
+    }else{
+        FirstCell = S->Next;
+        S->Next = FirstCell->Next;
+        free(FirstCell);
+    }
+}
+int
+main( int argc, char *argv[] )
+{
+	Stack A;
+    A=CreateStack();
+	return 0;
+}
